060b25fe554b58c92b6e11eae0b0369d8e0552ce
Eric Ekström <eric.ekstrm@gmail.com>
2016-04-19 12:02:15 +0000

    =börjat på design för movie-sidan

--- app/views/movies/movie.php
+++ app/views/movies/movie.php
@@ -8,29 +8,33 @@
         <link rel="stylesheet" type="text/css" href="/public/css/tiles.css">
     </head>
     <body>
-        <div id="header">
-            <a href="/public"><div>Home</div></a>
-            <a href="/public/movie"><div id="current">Movies</div></a>
-            <a href="/public/tvshows"><div>TV-shows</div></a>
-            <a id="login"><div>Login</div></a>
-        </div>
-        <div id="content">
-            <?php
-            echo "<img src='" . $data[0]["Poster"] . "' id='poster'>";
-            echo "<h1>" . $data[0]["Title"] . " (" . $data[0]["Year"] . ")</h1>";
-            ?>
-            <h2>Ratings</h2>
-            <ul>
-                <li>imdb</li>
-                <li>metascore</li>
-                <li>user score</li>
-            </ul>
-            <h2>People</h2>
-            <ul>
-                <li>actors</li>
-                <li>writers</li>
-                <li>director</li>
-            </ul>
+        <div id="wrapper">
+            <div id="header">
+                <a href="/public"><div>Home</div></a>
+                <a href="/public/movie"><div id="current">Movies</div></a>
+                <a href="/public/tvshows"><div>TV-shows</div></a>
+                <a id="login"><div>Login</div></a>
+            </div>
+            <div id="content">
+                <div id="topbox">
+                <?php
+                echo "<img src='" . $data[0]["Poster"] . "' id='poster'>";
+                ?>
+                <h1>Star Wars: Episode V - The Empire Strikes Back (1980)</h1>
+                </div>
+                <h2>Ratings</h2>
+                <ul>
+                    <li>imdb</li>
+                    <li>metascore</li>
+                    <li>user score</li>
+                </ul>
+                <h2>People</h2>
+                <ul>
+                    <li>actors</li>
+                    <li>writers</li>
+                    <li>director</li>
+                </ul>
+            </div>
         </div>
     </body>
 </html>
