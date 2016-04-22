47798f46340d34c51e346e52fc1d2dfc8d4c957c
Eric Ekström <eric.ekstrm@gmail.com>
2016-04-22 08:29:08 +0000

    =jobbat på designen

--- app/views/movies/movie.php
+++ app/views/movies/movie.php
@@ -4,23 +4,28 @@
         <title><?php echo $data[0]["Title"]?></title>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="/public/css/style.css">
-        <link rel="stylesheet" type="text/css" href="/public/css/movies.css">
+        <link rel="stylesheet" type="text/css" href="/public/css/movie.css">
         <link rel="stylesheet" type="text/css" href="/public/css/tiles.css">
     </head>
     <body>
+        <div id="header">
+            <a href="/public"><div>Home</div></a>
+            <a href="/public/movie"><div id="current">Movies</div></a>
+            <a href="/public/tvshows"><div>TV-shows</div></a>
+            <a id="login"><div>Login</div></a>
+        </div>
         <div id="wrapper">
-            <div id="header">
-                <a href="/public"><div>Home</div></a>
-                <a href="/public/movie"><div id="current">Movies</div></a>
-                <a href="/public/tvshows"><div>TV-shows</div></a>
-                <a id="login"><div>Login</div></a>
-            </div>
             <div id="content">
                 <div id="topbox">
-                <?php
-                echo "<img src='" . $data[0]["Poster"] . "' id='poster'>";
-                ?>
-                <h1>Star Wars: Episode V - The Empire Strikes Back (1980)</h1>
+                    <?php echo "<img src='" . $data[0]["Poster"] . "' id='poster'>"?>
+                    <div id="titlebox">
+                        <?php echo "<h1>" . $data[0]["Title"] . " (" . $data[0]["Year"] . ")</h1>"?>
+                        <hr>
+                        <?php echo "<h2>" . $data[0]["Runtime"] . " | " . $data[0]["ReleaseDate"] . "</h2>"?>
+                    </div>
+                    <div id="plot">
+                        <?php echo "<p>" . $data[0]["Plot"] . ")</p>"?>
+                    </div>
                 </div>
                 <h2>Ratings</h2>
                 <ul>
